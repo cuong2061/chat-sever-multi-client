1e158fef53ca22115d3e59767bfc7850ac758f43
Nguyen Cao Cuong <nguyencaocuong261@gmail.com>
2016-09-11 23:46:53 +0700

    merge vs master

---
+++ inc/header.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <stdio.h> // use perror function
+#include <stdlib.h> // use exit function
+#include <string.h> // use bzero function
+#include <netinet/in.h> // use struct sockaddr_in
+#include <unistd.h> // use close function
+#include <netdb.h> // use gethostbyaddr() function, use gethostbyname() function
+#include <iostream> // use cout, cin
+using namespace std;
+
+#define PORT_NUMBER 55555
+
+#endif /* _HEADER_H_ */
